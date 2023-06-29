05850bf885288bc6cb11352ec2c9ca149ebebda8
iqbaldf7 <iqbalfathonah10@gmail.com>
2023-06-29 16:47:04 +0700

    Menambahkan konstruktor kelas node

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -8,5 +8,12 @@ public:
 	Node* leftchild;
 	Node* rightchild;
 
-
+	// Constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
 };
+
